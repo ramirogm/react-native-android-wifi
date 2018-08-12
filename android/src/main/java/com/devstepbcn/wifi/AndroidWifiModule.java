145a56aef7540e7e284a970c270eec3e196441c8
Ramiro González Maciel <ramiro@snoopconsulting.com>
2018-08-12 17:22:06 -0300

    Update AndroidWifiModule.java

--- android/src/main/java/com/devstepbcn/wifi/AndroidWifiModule.java
+++ android/src/main/java/com/devstepbcn/wifi/AndroidWifiModule.java
@@ -74,7 +74,7 @@ public class AndroidWifiModule extends ReactContextBaseJavaModule {
             wifiObject.put("level", result.level);
             wifiObject.put("timestamp", result.timestamp);
             //Other fields not added
-            // FIXME Add other wifiObjects fields NOW_B
+            // FIXME Add other wifiObjects fields NOW_C
             //wifiObject.put("operatorFriendlyName", result.operatorFriendlyName);
             //wifiObject.put("venueName", result.venueName);
             //wifiObject.put("centerFreq0", result.centerFreq0);
